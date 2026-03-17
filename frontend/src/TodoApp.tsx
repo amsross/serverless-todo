e0cba9da30de7c13759e0ffa9afe894c258efba3
camerontheross <camerontheross@gmail.com>
2026-03-17 11:58:22 -0400

    feat: renamed app component and added simple todo layout

--- frontend/src/TodoApp.tsx
+++
@@ -1,9 +0,0 @@
-import { TaskCard } from "./components/TaskCard.tsx"
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-export const App = () => {
-  return (
-    <TaskCard title="foo" desc="bar" />
-  )
-}
