6cbc2b846d3d6f67c65b359c07bc63efb6dc439d
camerontheross <camerontheross@gmail.com>
2026-03-18 13:55:20 -0400

    feat(component): created task card react component

--- frontend/src/components/TaskCard.tsx
+++ frontend/src/components/TaskCard.tsx
@@ -1,18 +1,47 @@
-import Card from 'react-bootstrap/Card'
+import { OverlayTrigger, Popover, Button, Card, Row, Col } from 'react-bootstrap';
 
 interface TaskProps {
   title: string;
-  desc: string;
+  desc?: string;
 }
 
-export const TaskCard = ({ title, desc }: TaskProps) => {
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{desc}</Card.Text>
 
-      </Card.Body>
+const defaultTaskDescription: string = "No available description for this task"
+
+const descriptionPopover = (desc?: string) => (
+  <Popover>
+    <Popover.Header as="h3">DESCRIPTION</Popover.Header>
+    <Popover.Body>
+      {desc}
+    </Popover.Body>
+  </Popover>
+)
+
+
+export const TaskCard = ({ title, desc }: TaskProps) => {
+  console.log(desc)
+
+  return (
+    <Card className="Task-Card">
+      <Row>
+
+        <Col>
+          <Button variant="dark">{'<'}</Button>
+        </Col>
+
+        <Col>
+
+          <OverlayTrigger trigger="hover" overlay={descriptionPopover(desc || defaultTaskDescription)}>
+            <div className="Task-Name">{title}</div>
+          </OverlayTrigger>
+
+        </Col>
+
+        <Col>
+          <Button variant="dark">{'>'}</Button>
+        </Col>
+
+      </Row>
 
     </Card>
   )
