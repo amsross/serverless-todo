404c647d99b45d744ea6e46c19ae3259590ab76b
camerontheross <camerontheross@gmail.com>
2026-04-05 18:04:37 -0400

    feat: user can now sign in and out

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,24 +4,57 @@ import { Container, Stack } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
+import { useAuth } from "react-oidc-context"
+
+
 export const App = () => {
 
+  const auth = useAuth();
+  const signOutRedirect = () => {
+    const clientId = "6u3udafd9fqvlnnb368no3a2gd";
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://us-east-29qc8vavlu.auth.us-east-2.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+
+    return (
+      <div className="App">
+
+        <header className="App-header">
+          <h1>TO DO LIST</h1>
+        </header>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+
+        <Container>
+          <Stack direction='horizontal'>
+            <TodoList status="PENDING"></TodoList>
+            <TodoList status="DOING"></TodoList>
+            <TodoList status="DONE"></TodoList>
+          </Stack>
+        </Container>
+
+      </div>
+
+    )
+  }
+
   return (
-    <div className="App">
-
-      <header className="App-header">
-        <h1>TO DO LIST</h1>
-
-      </header>
-      <Container>
-        <Stack direction='horizontal'>
-          <TodoList status="PENDING"></TodoList>
-          <TodoList status="DOING"></TodoList>
-          <TodoList status="DONE"></TodoList>
-        </Stack>
-      </Container>
-
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
     </div>
-
-  )
+  );
 }
+
+
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.tsx'
+import { AuthProvider } from "react-oidc-context"
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_9QC8VAvLu",
+  client_id: "6u3udafd9fqvlnnb368no3a2gd",
+  redirect_uri: "https://d296kj58bcm6k.cloudfront.net/index.html",
+  response_type: "code",
+  scope: "email openid phone",
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App >
+      </App >
+    </AuthProvider>
   </StrictMode>,
 )
